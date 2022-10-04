53d2b7de6c8eed87c7eca121dfecf605492e3ce2
Nguyen Hai Dang <goingmyway243@gmail.com>
2022-10-04 22:31:31 +0700

    Update login

--- TruyenTranhWebApp-FE/src/app/app-routing.module.ts
+++ TruyenTranhWebApp-FE/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent }
+  { path: '', component: LoginPageComponent }
 ];
 
 @NgModule({
