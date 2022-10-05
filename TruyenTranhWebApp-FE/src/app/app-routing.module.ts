bde837b62b9677f7979a17d8003604f0c5b0d175
Đăng Nguyễn Hải <goingmyway243@gmail.com>
2022-10-05 23:56:17 +0700

    Update detail page

--- TruyenTranhWebApp-FE/src/app/app-routing.module.ts
+++ TruyenTranhWebApp-FE/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComicDetailPageComponent } from './pages/comic-detail-page/comic-detail-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 const routes: Routes = [
-  { path: '', component: LoginPageComponent }
+  { path: '', component: HomePageComponent },
+  { path: 'dang-nhap', component: LoginPageComponent },
+  { path: 'dang-ky', component: SignupPageComponent },
+  { path: 'truyen-tranh/:id', component: ComicDetailPageComponent }
 ];
 
 @NgModule({
