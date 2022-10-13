6838054c4603fb7440e89a3bdf1a37a8fd2eeb41
Đăng Nguyễn Hải <goingmyway243@gmail.com>
2022-10-13 23:37:56 +0700

    UPdate dashboard

--- TruyenTranhWebApp-FE/src/app/main/main.component.ts
+++ TruyenTranhWebApp-FE/src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private elmRef: ElementRef) { }
 
   ngOnInit(): void {
+    this.initAvatarHoverEvent();
   }
 
+  initAvatarHoverEvent(): void {
+    let hoverPanel = this.elmRef.nativeElement.querySelector('.hover-panel') as HTMLElement;
+    let avatar = this.elmRef.nativeElement.querySelector('.user-info') as HTMLElement;
+
+    avatar.addEventListener('mouseover', () => {
+      hoverPanel.style.display = 'flex';
+    });
+
+    avatar.addEventListener('mouseleave', () => {
+      hoverPanel.style.display = 'none';
+    });
+
+    hoverPanel.addEventListener('mouseover', () => {
+      hoverPanel.style.display = 'flex';
+    });
+
+    hoverPanel.addEventListener('mouseleave', () => {
+      hoverPanel.style.display = 'none';
+    });
+  }
 }
