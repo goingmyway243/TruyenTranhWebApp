28ccacfb6ba9f543ed68536630b1be00041bc878
Nguyen Hai Dang <goingmyway243@gmail.com>
2022-11-01 00:36:57 +0700

    Init edit remove

--- TruyenTranhWebApp-FE/src/app/admin/pages/admin-authors-page/admin-authors-page.component.ts
+++ TruyenTranhWebApp-FE/src/app/admin/pages/admin-authors-page/admin-authors-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthorModel } from 'src/app/models/author.model';
 import { AuthorService } from 'src/app/services/author.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-admin-authors-page',
@@ -13,7 +14,42 @@ export class AdminAuthorsPageComponent implements OnInit {
   constructor(private authorService: AuthorService) { }
 
   ngOnInit(): void {
+    this.getAllAuthor();
+  }
+
+  getAllAuthor(): void {
     this.authorService.getAll().subscribe(data => this.listAuthors = data);
   }
 
+  removeAuthor(id: number): void {
+    Swal.fire({
+      icon: 'question',
+      title: 'Đăng xuất',
+      text: `Bạn có chắc muốn xóa tác giả có mã '${id}'?`,
+      showCancelButton: true,
+      showConfirmButton: true,
+      focusCancel: true,
+      confirmButtonText: 'Xóa',
+      cancelButtonText: 'Không',
+      confirmButtonColor: 'var(--color-primary)',
+      cancelButtonColor: 'var(--color-danger)'
+    }).then(result => {
+      if (result.isConfirmed) {
+        this.authorService.delete(id).subscribe(
+          data => {
+            this.getAllAuthor();
+          },
+          error => {
+            console.log(error);
+            Swal.fire({
+              position: 'top-end',
+              icon: 'error',
+              title: 'Có lỗi xảy ra!',
+              showConfirmButton: false,
+              timer: 1500
+            });
+          });
+      }
+    });
+  }
 }
